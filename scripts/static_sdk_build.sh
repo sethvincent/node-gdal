c378ae6c403e6926d02402877de87cfc723d8342
Dane Springmeyer <dane@mapbox.com>
2014-11-13 18:48:18 -0800

    try with sdk built with clang

--- scripts/static_sdk_build.sh
+++ scripts/static_sdk_build.sh
@@ -2,12 +2,12 @@
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
     sudo apt-get -qq update
-    SDK_NAME="mapnik-linux-sdk-v2.2.0-622-gea61c57"
+    SDK_NAME="mapnik-linux-sdk-v3.0.0-rc1-43-g3cbde2d"
     curl -f -O -L https://mapnik.s3.amazonaws.com/dist/dev/${SDK_NAME}.tar.bz2
     tar xf ${SDK_NAME}.tar.bz2
     export PATH=$(pwd)/${SDK_NAME}/bin:$PATH
 elif [[ "$(uname -s)" == 'Darwin' ]]; then
-    SDK_NAME="mapnik-macosx-sdk-v2.2.0-622-gea61c57"
+    SDK_NAME="mapnik-macosx-sdk-v3.0.0-rc1-43-g3cbde2d-lto"
     curl -f -O -L https://mapnik.s3.amazonaws.com/dist/dev/${SDK_NAME}.tar.bz2
     tar xf ${SDK_NAME}.tar.bz2
     export PATH=$(pwd)/${SDK_NAME}/bin:$PATH
