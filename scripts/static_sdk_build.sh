1230f27db974032abfe61f214091bfcc8177161a
Dane Springmeyer <dane@mapbox.com>
2014-11-13 18:37:50 -0800

    update SDK

--- scripts/static_sdk_build.sh
+++ scripts/static_sdk_build.sh
@@ -2,13 +2,15 @@
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
     sudo apt-get -qq update
-    curl -f -O -L https://mapnik.s3.amazonaws.com/dist/dev/mapnik-linux-sdk-v3.0.0-rc1-40-g8cad5be.tar.bz2
-    tar xf mapnik-linux-sdk-v2.2.0-622-gea61c57.tar.bz2
-    export PATH=$(pwd)/mapnik-linux-sdk-v2.2.0-622-gea61c57/bin:$PATH
+    SDK_NAME="mapnik-linux-sdk-v2.2.0-622-gea61c57"
+    curl -f -O -L https://mapnik.s3.amazonaws.com/dist/dev/${SDK_NAME}.tar.bz2
+    tar xf ${SDK_NAME}.tar.bz2
+    export PATH=$(pwd)/${SDK_NAME}/bin:$PATH
 elif [[ "$(uname -s)" == 'Darwin' ]]; then
-    curl -f -O -L https://mapnik.s3.amazonaws.com/dist/dev/mapnik-macosx-sdk-v2.2.0-622-gea61c57.tar.bz2
-    tar xf mapnik-macosx-sdk-v2.2.0-622-gea61c57_.tar.bz2
-    export PATH=$(pwd)/mapnik-macosx-sdk-v2.2.0-622-gea61c57/bin:$PATH
+    SDK_NAME="mapnik-macosx-sdk-v2.2.0-622-gea61c57"
+    curl -f -O -L https://mapnik.s3.amazonaws.com/dist/dev/${SDK_NAME}.tar.bz2
+    tar xf ${SDK_NAME}.tar.bz2
+    export PATH=$(pwd)/${SDK_NAME}/bin:$PATH
 fi
 
 npm install --build-from-source --shared_gdal
