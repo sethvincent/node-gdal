7f063567eded87aa7c1d2c3585c43595de1ff1b7
Dane Springmeyer <dane@mapbox.com>
2014-11-13 18:35:02 -0800

    fix curl download

--- scripts/static_sdk_build.sh
+++ scripts/static_sdk_build.sh
@@ -2,11 +2,11 @@
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
     sudo apt-get -qq update
-    curl -f -o https://mapnik.s3.amazonaws.com/dist/dev/mapnik-linux-sdk-v3.0.0-rc1-40-g8cad5be.tar.bz2
+    curl -f -O -L https://mapnik.s3.amazonaws.com/dist/dev/mapnik-linux-sdk-v3.0.0-rc1-40-g8cad5be.tar.bz2
     tar xf mapnik-linux-sdk-v2.2.0-622-gea61c57.tar.bz2
     export PATH=$(pwd)/mapnik-linux-sdk-v2.2.0-622-gea61c57/bin:$PATH
 elif [[ "$(uname -s)" == 'Darwin' ]]; then
-    curl -f -o https://mapnik.s3.amazonaws.com/dist/dev/mapnik-macosx-sdk-v2.2.0-622-gea61c57.tar.bz2
+    curl -f -O -L https://mapnik.s3.amazonaws.com/dist/dev/mapnik-macosx-sdk-v2.2.0-622-gea61c57.tar.bz2
     tar xf mapnik-macosx-sdk-v2.2.0-622-gea61c57_.tar.bz2
     export PATH=$(pwd)/mapnik-macosx-sdk-v2.2.0-622-gea61c57/bin:$PATH
 fi
